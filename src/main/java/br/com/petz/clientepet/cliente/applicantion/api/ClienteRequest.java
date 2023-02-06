0c668968b90c90b7defcbd4f852c6a41daca7c59
Your Name <you@example.com>
2023-02-06 16:36:07 -0300

    implementa Request e Response

--- src/main/java/br/com/petz/clientepet/cliente/applicantion/api/ClienteRequest.java
+++ src/main/java/br/com/petz/clientepet/cliente/applicantion/api/ClienteRequest.java
@@ -1,5 +1,32 @@
 package br.com.petz.clientepet.cliente.applicantion.api;
+import br.com.petz.clientepet.cliente.domain.Sexo;
 
+import java.time.LocalDate;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import lombok.Value;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+@Value
 public class ClienteRequest {
-
+	
+	@NotBlank
+	private String nomeCompleto;
+	@NotBlank
+	@Email
+	private String email;
+	@NotBlank
+	private String celular;
+	private String telefone;
+	private Sexo sexo;
+	@NotNull
+	private LocalDate dataNascimento;
+	@CPF
+	private String cpf;
+	@NotNull
+	private Boolean aceitaTermos;
 }
