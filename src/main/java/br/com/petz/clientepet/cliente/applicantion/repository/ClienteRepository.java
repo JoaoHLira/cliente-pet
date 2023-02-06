8097d0235affa20876efbb82ff7af5a1c1a39a13
Your Name <you@example.com>
2023-02-06 17:38:06 -0300

    implementa ClienteApplicationService e o novo construtor Cliente

---
+++ src/main/java/br/com/petz/clientepet/cliente/applicantion/repository/ClienteRepository.java
@@ -0,0 +1,9 @@
+package br.com.petz.clientepet.cliente.applicantion.repository;
+
+import br.com.petz.clientepet.cliente.domain.Cliente;
+
+public interface ClienteRepository {
+
+	Cliente salva(Cliente cliente);
+
+}
