4f32c58b9c85e62226ac59308b275634873afe41
Your Name <you@example.com>
2023-02-06 16:15:36 -0300

    implementa controller e fluxo para service

---
+++ src/main/java/br/com/petz/clientepet/cliente/applicantion/service/ClienteService.java
@@ -0,0 +1,10 @@
+package br.com.petz.clientepet.cliente.applicantion.service;
+
+import br.com.petz.clientepet.cliente.applicantion.api.ClienteRequest;
+import br.com.petz.clientepet.cliente.applicantion.api.ClienteResponse;
+
+public interface ClienteService {
+
+	ClienteResponse criaCliente(ClienteRequest clienteRequest);
+
+}
