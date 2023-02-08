b6faca653f3d734ce9429b27dcc817cb07168c75
Your Name <you@example.com>
2023-02-08 16:48:19 -0300

    implementa fluxo de servico de implementacao

--- src/main/java/br/com/petz/clientepet/cliente/applicantion/api/ClienteController.java
+++ src/main/java/br/com/petz/clientepet/cliente/applicantion/api/ClienteController.java
@@ -1,11 +1,11 @@
 package br.com.petz.clientepet.cliente.applicantion.api;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.petz.clientepet.cliente.applicantion.service.ClienteService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
@@ -29,6 +29,15 @@ public class ClienteController implements ClienteAPI {
 		List<ClienteListResponse> clientes = clienteService.buscaTodosClientes();
 		log.info("[finaliza] ClienteController - getTodosClientes");
 		return clientes;
+	}
+
+	@Override
+	public ClienteDetalhadoResponse getClienteAtravesId(UUID idCliente) {
+		log.info("[inicia] ClienteController - getClienteAtravesId");
+		log.info("[idCliente] {}", idCliente);
+		ClienteDetalhadoResponse clienteDetalhado = clienteService.buscaClienteAtravesId(idCliente);
+		log.info("[finaliza] ClienteController - getClienteAtravesId");
+		return clienteDetalhado;
 	}
 
 }
