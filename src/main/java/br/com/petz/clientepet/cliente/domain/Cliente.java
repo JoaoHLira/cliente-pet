4367bc5ce4c8a63aca961133147f13263d98c4c7
Your Name <you@example.com>
2023-02-17 12:27:34 -0300

    implementa pet domain

--- src/main/java/br/com/petz/clientepet/cliente/domain/Cliente.java
+++ src/main/java/br/com/petz/clientepet/cliente/domain/Cliente.java
@@ -10,6 +10,8 @@ import br.com.petz.clientepet.cliente.applicantion.api.ClienteAlteracaoRequest;
 import br.com.petz.clientepet.cliente.applicantion.api.ClienteRequest;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -27,7 +29,7 @@ public class Cliente {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name = "id", updatable = false, unique = true, nullable = false)
+	@Column(columnDefinition = "uuid" ,name = "idCliente", updatable = false, unique = true, nullable = false)
 	private UUID idCliente;
 	@NotBlank
 	private String nomeCompleto;
@@ -38,6 +40,7 @@ public class Cliente {
 	@NotBlank
 	private String celular;
 	private String telefone;
+	@Enumerated(EnumType.STRING)
 	private Sexo sexo;
 	@NotNull
 	private LocalDate dataNascimento;
---
+++ src/main/java/br/com/petz/clientepet/pet/Porte.java
@@ -0,0 +1,5 @@
+package br.com.petz.clientepet.pet;
+
+public enum Porte {
+	PEQUENO, MEDIO, GRANDE;
+}
